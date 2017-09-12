e1df29df606cc1bed022e5dcf696b71705e0f9be
nbelyanskiy <31652170+nbelyanskiy@users.noreply.github.com>
2017-09-13 01:04:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
 <? echo 'Hello!'; ?>
+<svg width="120" height="120" viewBox="0 0 120 120"
+    xmlns="http://www.w3.org/2000/svg">
+
+  <line x1="20" y1="100" x2="100" y2="20"
+      stroke-width="2" stroke="black"/>
+</svg>
