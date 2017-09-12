118edd46393f66c7ec90ca95143981acd3ae8f85
nbelyanskiy <31652170+nbelyanskiy@users.noreply.github.com>
2017-09-13 01:34:22 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -3,10 +3,13 @@ var str='Hello!';
 $(document).ready(function(){
 
 var svgDraw = '<svg width="120" height="120" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">';
-  var x=0, y=0;
+  var x2=0, y2=0, x1=0, y1=0;
   for (var i=0; i<100; i++) { 
-    y=(2^x)-1;
-    svgDraw += '<line x1="20" y1="100" x2="100" y2="20" stroke-width="2" stroke="black"/>';
+    x1 = x2;
+    y1 = y2;
+    y2=(2^x2)-1;
+    svgDraw += '<line x1="‘+x1+‘" y1="‘+y1+‘" x2="‘+x2+‘" y2="‘+y2+‘" stroke-width="1" stroke="black"/>';
+    x=x + 0.1;
   };
 svgDraw += '</svg>';
 $('#test').html(svgDraw);
