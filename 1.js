0d12a64f7982d10139af52e0d1a85af230feaa53
nbelyanskiy <31652170+nbelyanskiy@users.noreply.github.com>
2017-09-13 01:20:42 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,2 +1,10 @@
 'use strict';
 var str='Hello!';
+$(‘#test').html(svgDraw);
+var svgDraw = '<svg width="120" height="120" viewBox="0 0 120 120" xmlns="http://www.w3.org/2000/svg">';
+  var x=0, y=0;
+  for (var i=0; i<100; i++) { 
+    y=(2^x)-1;
+    svgDraw += '<line x1="20" y1="100" x2="100" y2="20" stroke-width="2" stroke="black"/>';
+  };
+svgDraw += '</svg>';
